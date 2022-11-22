44e4ab314d9f76c31e7095cbc329ab7de41378dd
dragan.ribes <dragan.ribes@epita.fr>
2022-11-22 01:42:34 +0100

    added a tool for contain a roll

---
+++ src/main/java/src/main/java/tools/RollResult.java
@@ -0,0 +1,9 @@
+package src.main.java.tools;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class RollResult {
+    StringBuilder stringBuilder;
+    float aFloat; // This is a float from 0 to 1
+}
